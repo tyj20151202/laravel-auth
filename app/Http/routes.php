93614d294a670fc25a2745960eacc95a25932768
TY <495789532@qq.com>
2019-01-27 16:42:34 +0800

    working to 5.1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,3 +22,9 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 //register
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email','Auth\PasswordController@postEmail');
+
+Route::get('password/reset/{token}','Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
